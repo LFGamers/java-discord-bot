cd1d1a3a4ceb91a76a824a3b40e3f53a37ea38d9
Aaron Scherer <aequasi@gmail.com>
2016-05-31 13:50:16 -0700

    Updating deps

---
+++ src/Model/Permission.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of discord-bot.
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace LFGamers\Discord\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ *
+ * User Class
+ * @ORM\Entity
+ * @ORM\Table(name="permission")
+ */
+class Permission
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Permission
+     */
+    public function setId(int $id) : Permission
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Permission
+     */
+    public function setName(string $name) : Permission
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Model/Role.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * This file is part of discord-bot.
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace LFGamers\Discord\Model;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ *
+ * User Class
+ * @ORM\Entity
+ * @ORM\Table(name="role")
+ */
+class Role
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var int
+     * @ORM\Column(type="bigint")
+     */
+    protected $identifier;
+
+    /**
+     * @var ArrayCollection|Permission[]
+     *
+     * @ORM\ManyToMany(targetEntity="Permission")
+     * @ORM\JoinTable(
+     *      name="role_permissions",
+     *      joinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="permission_id", referencedColumnName="id", unique=true)}
+     *  )
+     */
+    protected $permissions;
+
+    public function __construct()
+    {
+        $this->permissions = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Role
+     */
+    public function setId(int $id) : Role
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdentifier() : int
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @param int $identifier
+     *
+     * @return Role
+     */
+    public function setIdentifier(int $identifier) : Role
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * @param array $permissions
+     *
+     * @return Role
+     */
+    public function setPermissions(array $permissions) : Role
+    {
+        $this->permissions = new ArrayCollection($permissions);
+
+        return $this;
+    }
+}
